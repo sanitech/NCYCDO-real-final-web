110002215c68d955c4000cc9f91af5f99360f3d4
sanitech <sanitech47@gmail.com>
2024-02-17 01:58:45 +0300

    deploy build

--- src/Components/Navbar/NavTwo.jsx
+++ src/Components/Navbar/NavTwo.jsx
@@ -310,7 +310,7 @@ function NavTwo() {
                   class="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
                 >
                   <div class="font-semibold">
-                    Young Girls and Women Empowerment{" "}
+                    Young Girls and Women Empowerment
                   </div>
                   <span class="text-sm text-gray-500 dark:text-gray-400">
                     Connect with third-party tools that you're already using.
